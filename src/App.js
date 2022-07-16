bd286b403003d83f4c10b03f92562e50c029f185
Luis Hdz <joseluishdz02@gmail.com>
2022-07-15 22:00:52 -0500

    feat: modify the App file with the API

--- src/App.js
+++ src/App.js
@@ -1,45 +1,94 @@
+import axios from 'axios';
+import { useEffect, useRef, useState } from 'react';
 import './App.css';
-import axios from 'axios'
-import { useEffect, useState } from 'react';
-import PokemonList from './PokemonList';
+import { BuscadorProvider, BuscadorConsumer, Result } from './context/pokeContext';
+import { useTranslation } from 'react-i18next';
+
+export default () => <div>
+  <BuscadorProvider>  
+    <BuscadorConsumer/>
+    <App/>
+  </BuscadorProvider>
+</div>
 
 function App() {
-  const [pokemon, setPokemon] = useState([])
-  const [currentUrl, setCurrentUrl] = useState("https://pokeapi.co/api/v2/pokemon?&limit=10")
-  const [prevUrl, setPrevUrl] = useState()
-  const [nextUrl, setNextUrl] = useState()
+  const [pokemon, setPokemon] = useState([]);   
+  const [infoPok, setInfoPok] = useState([]);   
+  const [array, setArray] = useState([]);
+  const [prevUrl, setPrevUrl] = useState(0);
+  const [nextUrl, setNextUrl] = useState(10); 
+  const info = Result();
+  const message = useRef(null);
+  const { t } = useTranslation(['translate']);
+
+  useEffect(() => {    
+    axios.get(`https://pokeapi.co/api/v2/pokemon?limit=1126`).then(data => {      
+      setInfoPok(data.data.results);                             
+    });        
+  }, []);
 
   useEffect(() => {
-    axios.get(currentUrl)
-      .then(res => { 
-        setNextUrl(res.data.next)
-        setPrevUrl(res.data.previous)
-        setPokemon(res.data.results)
-  })
-    }, [currentUrl]);
-
-    const Pagination = (e) => {
-      if(e.target.textContent === "NEXT"){
-        setCurrentUrl(nextUrl)
-      }
-      if(e.target.textContent === "PREVIOUS"){
-        setCurrentUrl(prevUrl)
-      }
+    if(infoPok.length > 0){
+      infoPok.slice(prevUrl, nextUrl).filter((elemento) => {
+        if(elemento.name.toString().toLowerCase().includes(info.search)){
+          setArray([]);
+          const url = elemento.url.split('/');
+          axios.get(`https://pokeapi.co/api/v2/pokemon/${url[6]}`).then(data => {
+            setArray((current) => [...current, {data: data.data}]);            
+          });
+        }
+      });    
     }
+  }, [info]);
+
+  useEffect(() => {
+    pokeChange();
+  }, [infoPok]);
+
+  const pokeChange = () => {
+    if(infoPok.length === 1126){
+      var position = infoPok.slice(prevUrl, nextUrl);
+      position.map(m => {
+        const url = m.url.split('/');
+        axios.get(`https://pokeapi.co/api/v2/pokemon/${url[6]}`).then(poke => {   
+          setArray((current) => [...current, {data: poke.data}]);          
+        }).catch(function (e) {
+          console.log(e.message);
+        });
+      });
+    }
+  }
+
+  useEffect(() => {   
+    setArray([]);     
+    infoPok.slice(prevUrl, nextUrl).map(info => {
+      const url = info.url.split('/');      
+      axios.get(`https://pokeapi.co/api/v2/pokemon/${url[6]}`).then(poke => {      
+        setArray((current) => [...current, {data: poke.data}]);       
+      }).catch(function (e) {
+        console.log(e.message);
+      });
+      setPokemon(array);     
+    });            
+  }, [prevUrl]);
 
   return (
     <div className="App">
-      {pokemon.map(pokemon => 
-        <PokemonList pokemon = {pokemon} />
-        )}
-      <div className="btn">
-        <button onClick={Pagination} className="previous">PREVIOUS</button>
-        <button onClick={Pagination} className="next">NEXT</button>
+      <div className='AppPok'>
+      {
+         array.slice(0, 10).map((info, index) => (
+          <div className='pokeCard' key={index}>
+            <div title={info.data.name} placement="top">
+              <img onClick={(e) => {window.location.href = `/pokemons/${info.data.id}`}} className='img' src={info.data.sprites.front_default} alt=" " ></img>                           
+            </div>
+          </div>
+        ))
+      }
+        <div className="btn">
+          <button onClick={(e) => { setPrevUrl(prevUrl - 10); setNextUrl(nextUrl - 10); }} className="previous">{t("previous")}</button>
+          <button onClick={(e) => { setPrevUrl(prevUrl + 10); setNextUrl(nextUrl + 10); }} className="next">{t("next")}</button>
+        </div>
       </div>
     </div>
   );
 }
-
-
-
-export default App;
